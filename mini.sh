0f9ad6f2c257b2e08c5c2b49a0789bf356183c5f
Chameleon Cloud User <cc@minivite-2.novalocal>
2022-10-18 02:06:52 +0000

    update mini.sh

--- mini.sh
+++ mini.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # dataset 
-for dataset in com-orkut.ungraph.bin 
+for dataset in webbase-2001.bin
 do 
   # for nodes 1 - 20
-  for node in $(seq 17 20)
+  for node in $(seq 1 20)
   do
     echo "running ${node} nodes on ${dataset}"
     mpiexec -n ${node} ./miniVite -f ${dataset} > ./outputs/${node}_${dataset}.out
